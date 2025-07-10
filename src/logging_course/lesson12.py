c26625ab8d78fe412b74ef10d2300b7351e5bfe9
Luiz Otávio <luizomf@gmail.com>
2025-07-10 12:37:23 -0300

    aula 12

---
+++ src/logging_course/lesson12.py
@@ -0,0 +1,27 @@
+# Aula 12
+# QueueHandler e QueueListener: log sem travar sua aplicação - Aula 12
+# https://youtu.be/jX4Ai-ZWkj4
+#
+# Playlist:
+# https://www.youtube.com/playlist?list=PLbIBj8vQhvm28qR-yvWP3JELGelWxsxaI
+#
+# Artigo:
+#
+# https://www.otaviomiranda.com.br/2025/logging-no-python-pare-de-usar-print-no-lugar-errado/#queuehandler-queuelistener
+
+import logging
+
+from logging_course import config_logging
+
+if __name__ == "__main__":
+    config_logging.setup()
+
+    logger = logging.getLogger("meuapp")
+
+    logger.debug("mensagem de log")
+    logger.info("mensagem de log")
+    logger.warning("mensagem de log")
+    logger.error("mensagem de log")
+    logger.critical(
+        "Meu nome é %s %s", "Luiz", "Otávio", extra={"contexto": "Qualquer coisa"}
+    )
