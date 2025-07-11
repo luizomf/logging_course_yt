022ef3ae1be3e912d7931023a4598cffaa0641ec
Luiz Otávio <luizomf@gmail.com>
2025-07-11 19:56:20 -0300

    aula 13

---
+++ src/logging_course/final_main.py
@@ -0,0 +1,18 @@
+from logging_course.final_config_logging import get_logger
+
+
+def run() -> None:
+    logger = get_logger("meuapp")
+    logger = get_logger("meuapp")
+    logger = get_logger("meuapp")
+    logger = get_logger("meuapp")
+    logger = get_logger("meuapp")
+    logger = get_logger("meuapp")
+    logger = get_logger("meuapp")
+    logger = get_logger("meuapp")
+    logger.debug("Testando pela primeira vez")
+    print("Hello world")
+
+
+if __name__ == "__main__":
+    run()
